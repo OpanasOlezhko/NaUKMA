ee95a17bb83b5b9500b175fe44f1b410a1bd8560
maxlshk <max.loshak.i@gmail.com>
2023-04-11 17:43:37 +0300

    set/get

--- 1course/ProgrammingEntry/Trim2Lab2/src/Goods.java
+++ 1course/ProgrammingEntry/Trim2Lab2/src/Goods.java
@@ -1,2 +1,24 @@
 public class Goods {
+
+    private String name;
+    private String description;
+    private String provider;
+    private int amount;
+    private double price;
+
+
+    /**
+     * @author MaxLoshak
+     */
+    public void setName(String name) {this.name = name;}
+    public void setDescription(String description) {this.description = description;}
+    public void setProvider(String provider) {this.provider = provider;}
+    public void setAmount(int amount) {this.amount = amount;}
+    public void setPrice(double price) {this.price = price;}
+    public String getName() {return this.name;}
+    public String getDescription(){return this.description;}
+    public String getProvider() {return this.provider;}
+    public int getAmount() {return this.amount;}
+    public double getPrice() {return this.price;}
+
 }
--- 1course/ProgrammingEntry/Trim2Lab2/src/GoodsGroup.java
+++ 1course/ProgrammingEntry/Trim2Lab2/src/GoodsGroup.java
@@ -1,2 +1,14 @@
 public class GoodsGroup {
+
+    private String name;
+    private String description;
+
+
+    /**
+     * @author MaxLoshak
+     */
+    public void setName(String name) {this.name = name;}
+    public void setDescription(String description) {this.description = description;}
+    public String getName() {return this.name;}
+    public String getDescription() {return this.description;}
 }
